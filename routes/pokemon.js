59bcee88156675ca61ce4c244b0cd24796488db3
Rome Bell <romebell5@gmail.com>
2020-08-10 09:30:24 -0700

    [routes] Add delete route

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -8,9 +8,29 @@ router.get('/', function(req, res) {
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', function(req, res) {
-  // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+// router.post('/', function(req, res) {
+//   // TODO: Get form data and add a new record to DB
+//   res.send(req.body);
+// });
+
+router.post('/', (req, res) => {
+  db.pokemon.create({
+    name: req.body.name
+  }).then(() => {
+    res.redirect('/pokemon');
+  }).catch(err => {
+    console.log('Error', err);
+  });
+});
+
+router.delete('/:pokemon', (req, res) => {
+  console.log(req);
+  db.pokemon.destroy({
+    where: { name: req.params.pokeName }
+  })
+  .then(() => {
+    res.redirect('/pokemon');
+  });
 });
 
 module.exports = router;
